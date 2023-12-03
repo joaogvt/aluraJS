1d0b32a65c6d066d3bed299b2508bc79cb1ad959
joaogvt <sotorivarossi@gmail.com>
2023-12-03 00:42:04 -0300

    exemplos aula 2

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+//tipo de dado
+//booleans
+
+//conversao implicita
+const num = 5;
+const numString = Number('5');
+
+//Number()
+//String()
+console.log(num + numString);
+
+
+//conversao explicita
---
+++ truthyFalsy.js
@@ -0,0 +1,22 @@
+//boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+//0 => false
+//1 => true
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+
+//undefined
+
+//null => vazio | nada
+let minhaVar;
+let varNull = null;
+
+let num = 3;
+let text = 'Alura';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
